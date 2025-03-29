5262c3fc83cd6ab5a6c8574dcdf6baa7056f75ce
Darllan Moreira <darllanmoreira@Darllans-MacBook-Pro.local>
2025-03-29 16:34:32 -0300

    Adiciona rota principal ao webhook

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
-// Rota básica
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Webhook rodando com sucesso!');
 });
 
-// Escutando na porta fornecida pelo Render ou 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);

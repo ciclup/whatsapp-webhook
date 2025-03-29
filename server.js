1afa416f01ed5ec1d146bb1e08502874db4d2c9e
Darllan Moreira <darllanmoreira@Darllans-MacBook-Pro.local>
2025-03-29 16:42:25 -0300

    Adiciona verificação de token para o webhook

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.json());
+const token = "ciclup123"; // Coloque o mesmo token do painel Meta aqui
 
-app.get('/', (req, res) => {
-  res.send('Webhook rodando com sucesso!');
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const challenge = req.query["hub.challenge"];
+  const verify_token = req.query["hub.verify_token"];
+
+  if (mode === "subscribe" && verify_token === token) {
+    console.log("WEBHOOK_VERIFICADO");
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Webhook rodando com sucesso!");
 });
 
 const PORT = process.env.PORT || 3000;
